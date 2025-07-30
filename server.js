196e24c6052b1959744e62bcf996f8b172c96e07
keerthi123345 <165142270+keerthi123345@users.noreply.github.com>
2025-07-30 21:22:25 +0530

    Uploading complete project files

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    res.send(`<h2>Login Info</h2><p>Username: ${username}</p><p>Password: ${password}</p>`);
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
